3010f6303f73dbff5d9fc914d0ae4a4750ab22fa
MarceStarlet <marcestarlet@techwomen.org.mx>
2016-03-01 00:03:42 -0600

    HashTable

---
+++ java/src/main/java/org/marcestarlet/datastructures/HashTable.java
@@ -0,0 +1,26 @@
+package org.marcestarlet.datastructures;
+
+/**
+ * HashTable 
+ * Separate chaining with linked list to prevent collisions
+ * HashTable for an HashEntry<int,T>
+ * @author MarceStarlet
+ * Feb 29, 2016
+ * @param <T>
+ */
+public interface HashTable<T> {
+
+	/**
+	 * get value from the key given
+	 * @param key
+	 * @return
+	 */
+	T get(int key);
+	
+	/**
+	 * put the value for the key given
+	 * @param key
+	 * @param value
+	 */
+	void put(int key, T value);
+}
---
+++ java/test/main/java/org/marcestarlet/datastructures/HashTableImpTest.java
@@ -0,0 +1,67 @@
+package org.marcestarlet.datastructures;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class HashTableImpTest {
+
+	@Test
+	public void simpleTest() {
+		// Simple test
+		HashTable<String> ht = new HashTableImp<>();
+		
+		ht.put(1, "Marce");
+		ht.put(2, "Juan");
+		ht.put(3, "Ana");
+	
+		assertEquals("Juan", ht.get(2));
+	}
+	
+	@Test
+	public void repeatedHashCodeTest(){
+		// different keys same hash code
+        HashTable<String> ht = new HashTableImp<>();
+		
+		ht.put(1, "Marce");
+		ht.put(2, "Juan");
+		ht.put(3, "Ana");
+		ht.put(130, "Pedro");
+		
+		assertEquals("Pedro", ht.get(130));
+		
+	}
+	
+	@Test
+	public void differentValuesTest(){
+		// same keys different values
+        HashTable<Float> ht = new HashTableImp<>();
+		
+		ht.put(1, 0.1f);
+		ht.put(2, 0.2f);
+		ht.put(3, 0.3f);
+		ht.put(130, 0.130f);
+		ht.put(2, 0.32f);
+		
+		assertEquals(new Float(0.32), ht.get(2));
+		
+	}
+	
+	@Test
+	public void randomHashCodeTest(){
+		// TO-DO
+	}
+	
+	@Test
+	public void toStringTest(){
+		
+		HashTable<String> ht = new HashTableImp<>();
+		
+		ht.put(1, "Marce");
+		ht.put(2, "Juan");
+		ht.put(3, "Ana");
+		ht.put(130, "Pedro");
+		
+		assertEquals("HashTable=[<1,Marce>,<2,Juan>,<130,Pedro>,<3,Ana>]", ht.toString());
+	}
+
+}
